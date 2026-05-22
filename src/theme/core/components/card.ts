42c99df1afeca23ac3d664c745ce772a8c303bf2
thatonevikash <vikashloharjsr@gmail.com>
2026-05-22 22:39:10 +0530

    fix: removed unnecessary Theme inferring from **MuiCard**

--- src/theme/core/components/card.ts
+++ src/theme/core/components/card.ts
@@ -6,7 +6,7 @@ import type {} from "@mui/material/themeCssVarsAugmentation";
 
 const MuiCard: Components<Theme>["MuiCard"] = {
   styleOverrides: {
-    root: ({ theme }: { theme: Theme }) => ({
+    root: ({ theme }) => ({
       borderRadius: theme.spacing(2.5),
       boxShadow: `-4px -4px 24px 4px rgba(20, 22, 21, 0.04),
                 4px 12px 12px 4px rgba(20, 22, 21, 0.04)`,
